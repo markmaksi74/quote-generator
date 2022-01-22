75f47c74cfae16e599dc4b40d8f73d3a9b63a6e6
Mark Maksi <68314514+markmaksi74@users.noreply.github.com>
2022-01-22 07:27:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,66 @@
-const quoteContainer = document.getElementById('quote-container');
-const quoteText = document.getElementById('quote');
-const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote');
-const loader = document.getElementById('loader');
-
-// Show Loading
+// Show loader
 function loading() {
-    loader.hidden = false;
-    quoteContainer.hidden = true;
+  const loader = document.getElementById('loader');
+  const quoteContainer = document.getElementById('quote-container');
+  quoteContainer.setAttribute('hidden', '');
+  loader.removeAttribute('hidden', '');
 }
 
 // Hide Loading
-function complete() {
-    if (!loader.hidden) {
-        quoteContainer.hidden = false;
-        loader.hidden = true;
-    }
+function completeLoading() {
+  const loader = document.getElementById('loader');
+  const quoteContainer = document.getElementById('quote-container');
+  loader.setAttribute('hidden', '');
+  quoteContainer.removeAttribute('hidden', '');
 }
 
-// Get Quote From API
-async function getQuote() {
-    loading();
-    const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-    try {
-        const response = await fetch(proxyUrl + apiUrl);
-        const data = await response.json();
-        // If Author is blank, add 'Unknown'
-        if (data.quoteAuthor === '') {
-            authorText.innerText = 'Unknown';
-        } else {
-            authorText.innerText = data.quoteAuthor;
-        }
-        // Reduce font size for long quotes
-        if (data.quoteText.length > 120) {
-            quoteText.classList.add('long-quote');
-        } else {
-            quoteText.classList.remove('long-quote');
-        }
-        quoteText.innerText = data.quoteText;
-        // Stop Loader, Show Quote
-        complete();
-    } catch (error) {
-        getQuote();
-    }
+// Get quotes from API
+async function getAllQuotes() {
+  const API_URL = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch(API_URL);
+    const quotesArray = response.json();
+    return quotesArray;
+  } catch (error) {
+    console.log(error);
+    getAllQuotes();
+  }
 }
 
-// Tweet Quote
-function tweetQuote() {
-    const quote = quoteText.innerText;
-    const author = authorText.innerText;
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
-    window.open(twitterUrl, '_blank');
+// Picking one quote
+async function pickNewQuote() {
+  const quotesArray = await getAllQuotes();
+  const quote = quotesArray[Math.floor(Math.random() * quotesArray.length)];
+  return quote;
 }
 
-// Event Listeners
-newQuoteBtn.addEventListener('click', getQuote);
-twitterBtn.addEventListener('click', tweetQuote);
+/* Don't use event listneres to the global context 
+/* adding DOM elements as constants inside functions is best practice to force garbage collection by JS */
 
-// On Load
-getQuote();
+// Populating the quote to the UI
+async function populateQuote() {
+  loading();
+  // awaiting for the random quote to be ready....
+  const quote = await pickNewQuote();
+  const quoteText = document.getElementById('quote');
+  const quoteAuthor = document.getElementById('author');
+  if (!quoteAuthor) {
+    quoteAuthor.textContent = 'Unknown';
+  }
+  quoteAuthor.textContent = quote.author;
+  if (quoteText > 120) {
+    quoteText.classList.add('long-quote');
+  }
+  quoteText.classList.remove('long-quote');
+  quoteText.textContent = quote.text;
+  completeLoading();
+  return quote;
+}
 
+// Twitter button
+async function tweetQuote() {
+  const quote = await populateQuote();
+  const twitterURL = `https:twitter.com/intent/tweet?text=${quote.text} ~${quote.author}`;
+  window.open(twitterURL, '_blank');
+  return true;
+}
